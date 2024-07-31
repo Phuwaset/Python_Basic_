1f5f4ce52301c334e58695cd6492a86fe4ffb543
9'Phu the Magician <100694995+Phuwaset@users.noreply.github.com>
2024-07-31 15:46:45 +0700

    Create hex_def.py

---
+++ hex_def.py
@@ -0,0 +1,10 @@
+def cal(a, b):
+    return a + (b / 2) * 10
+
+name = input("Enter your name: ")
+a, b = map(float, input("Enter values for a and b separated by a space: ").split())
+
+result = f"{cal(a, b):,.2f}"
+a_hex, b_hex = hex(int(a)).upper(), hex(int(b)).upper()
+
+print(f'My name is: "{name}" B = {b:.2f} C = {a:.0f} \nResult: {result} from Hex (\'{a_hex}\', \'{b_hex}\')')
