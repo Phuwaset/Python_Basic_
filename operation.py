1f73571acd701c96c2bd3d99f825c04b6a57518c
9'Phu the Magician <100694995+Phuwaset@users.noreply.github.com>
2024-08-07 15:41:50 +0700

    Update operation.py
    update

--- operation.py
+++ operation.py
@@ -1,29 +1,35 @@
 import math
 
 def cal_check(x, y, z):
-    
-    operations = [
-        ((x >> 8) + y) ** z,  
-        (x & y) % z,          
-        round(x / y) >> z,    
-        math.ceil((2 + x) / y) | z  
+    rs1 = ((x >> 8) + y) ** z  
+    rs2 = (x & y) % z          
+    rs3 = round(x / y) >> z    
+    rs4 = math.ceil((2 + x) / y) | z 
+
+    total_sum = rs1 + rs2 + rs3 + rs4
+
+  
+    results = [
+        f"Cal ((x >> 8) + y) ** z = {rs1}",
+        f"Cal (x & y) % z = {rs2}",
+        f"Cal round(x / y) >> z = {rs3}",
+        f"Cal ((2 + x) / y) | z = {rs4}",
+        # f"Sum of all rs: {total_sum}"
     ]
 
-    total_sum = sum(operations)
-    
-    results = [f"rs {i+1}: {op}" for i, op in enumerate(operations)]
-    results.append(f"Sum of all rs: {total_sum}")
-
     conditions = {
-        "cd not ( x=y ) or (y<z)": not (x == y) | (y < z),
-        "cd x != y << z": x != (y << z)
+        "Check not (x = y) or (y < z)": not (x == y) | (y < z),
+        "Check x != y << z": x != (y << z)
     }
 
+  
     for result in results:
         print(result)
 
+  
     for cond, is_true in conditions.items():
         print(f"{cond} = {is_true}")
 
 values = [int(input(f"input {var}: ")) for var in ['x', 'y', 'z']]
+
 cal_check(*values)
