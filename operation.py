345d6154fbf38be15e3428b1d34b205f7d7fa4ef
9'Phu the Magician <100694995+Phuwaset@users.noreply.github.com>
2024-08-07 16:39:04 +0700

    Update operation.py
    update

--- operation.py
+++ operation.py
@@ -1,32 +1,25 @@
 import math
 
 def cal_check(x, y, z):
-    rs1 = ((x >> 8) + y) ** z  
+    rs1 = ((x >> 8) + y) ** z
     rs2 = (x & y) % z          
     rs3 = round(x / y) >> z    
-    rs4 = math.ceil((2 + x) / y) | z 
+    rs4 = math.ceil((2 + x) / y) | z  
 
-    total_sum = rs1 + rs2 + rs3 + rs4
-
-  
     results = [
-        f"Cal ((x >> 8) + y) ** z = {rs1}",
-        f"Cal (x & y) % z = {rs2}",
-        f"Cal round(x / y) >> z = {rs3}",
-        f"Cal ((2 + x) / y) | z = {rs4}",
-        # f"Sum of all rs: {total_sum}"
+        f"Cal ((x >> 8) + y) ** z = {rs1:.2f} ({int(rs1)})",
+        f"Cal (x & y) % z = {rs2} ({int(rs2)})",
+        f"Cal round(x / y) >> z = {rs3} ({int(rs3)})",
+        f"Cal ((2 + x) / y) | z = {rs4} ({int(rs4)})",
     ]
-
     conditions = {
-        "Check not (x = y) or (y < z)": not (x == y) | (y < z),
+        "Check not ( x=y ) or (y<z)": not (x == y) | (y < z),
         "Check x != y << z": x != (y << z)
     }
 
-  
     for result in results:
         print(result)
 
-  
     for cond, is_true in conditions.items():
         print(f"{cond} = {is_true}")
 
